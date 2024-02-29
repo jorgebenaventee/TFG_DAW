4923dfd009069f77089323d3298f5315e4758cb4
Jorge Benavente <jorge.benavente@alumno.iesluisvives.org>
2024-02-29 18:20:18 +0100

    Add dark mode

--- tfg-frontend/src/main.tsx
+++ tfg-frontend/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { ThemeSwitcher } from '@/components/theme-switcher.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <ThemeSwitcher />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
