7eb3a7729b5c37b4fb9e592adf35ce64adcad18a
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-05-04 12:32:37 +0200

    Add dynamic topbar

--- tfg-frontend/src/components/topbar.tsx
+++ tfg-frontend/src/components/topbar.tsx
@@ -1,12 +1,19 @@
+import { Link, useRouterState } from '@tanstack/react-router'
 import { Separator } from './ui/separator'
 import { ThemeSwitcher } from '@/components/theme/theme-switcher.tsx'
 
 export function Topbar() {
+  const router = useRouterState()
+
+  const isBoardRoute = () => router.location.pathname.includes('board')
   return (
     <>
       <header className="flex w-full justify-between">
         <div className="flex-1">
-          <header className="p-4">Soy un topbar :)</header>
+          <header className="flex gap-3 p-4">
+            <Link to="/">Mis tableros</Link>
+            {isBoardRoute() && <span>Ajustes del tablero</span>}
+          </header>
         </div>
         <div className="flex items-center gap-3 px-3">
           <ThemeSwitcher />
